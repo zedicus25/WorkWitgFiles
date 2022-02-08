071c800f1aa25eabe68a527364e0533ea3466ac2
Alexander Tsyganok <Ciga_fu80@student.itstep.org>
2022-02-08 20:04:38 +0200

    Demonstrated result

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,18 @@
 ﻿#include "c_WorkFiles.h"
 
+using std::cout;
+using std::cin;
 int main()
 {
 	std::auto_ptr<c_WorkFiles> file(new c_WorkFiles());
+	file->setPath("Data.txt");
+	file->print();
+	cout << "--------------------------\n";
+	cout << file->findString("Of") << "\n";
+	cout << "--------------------------\n";
+	file->replaseString("on", "ON");
+	file->print();
+	cout << "--------------------------\n";
+	file->revert();
+	file->print();
 }
--- ConsoleApplication1/c_WorkFiles.cpp
+++ ConsoleApplication1/c_WorkFiles.cpp
@@ -9,75 +9,76 @@ void c_WorkFiles::setPath(std::string path)
 
 int c_WorkFiles::findString(std::string str)
 {
+	this->file.close();
+	this->file.open(this->path.c_str());
 	size_t count = 0;
-	if (this->file.is_open()) {
-		size_t pos = 0;
-		std::string tmp;
-		while (!this->file.eof())
+	size_t pos = 0;
+	std::string tmp;
+	while (!this->file.eof())
+	{
+		std::getline(file, tmp);
+		while ((pos = tmp.find(str)) != std::string::npos)
 		{
-			std::getline(file, tmp);
-			while ((pos = tmp.find(str)) != std::string::npos)
-			{
-				tmp.erase(0, pos + str.length());
-				count++;
-			}
+			tmp.erase(0, pos + str.length());
+			count++;
 		}
 	}
+	
 	return count;
 }
 
 void c_WorkFiles::replaseString(std::string oldStr, std::string newStr)
 {
-	if (this->file.is_open()) {
-		std::string tmp;
-		char ch;
+	this->file.close();
+	this->file.open(this->path.c_str());
+	std::string tmp;
+	char ch;
 
-		while (this->file.get(ch)) {
-			tmp += ch;
-		}
-
-		size_t i = tmp.find(oldStr, 0);
-		while (i != std::string::npos) {
-			tmp.replace(i, newStr.length(), newStr);
-			i = tmp.find(oldStr, i);
-		}
-
-		this->file.clear();
-		this->file.seekg(0, std::ios::beg);
-
-		this->file << tmp;
+	while (this->file.get(ch)) {
+		tmp += ch;
 	}
+
+	size_t i = tmp.find(oldStr, 0);
+	while (i != std::string::npos) {
+		tmp.replace(i, newStr.length(), newStr);
+		i = tmp.find(oldStr, i);
+	}
+	this->file.close();
+	this->file.open(this->path.c_str());
+
+	this->file << tmp;
 }
 
 void c_WorkFiles::print()
 {
-	if (this->file.is_open()) {
-		std::string tmp;
-		while (!this->file.eof())
-		{
-			std::getline(file, tmp);
-			std::cout << tmp << "\n";
-		}
+	this->file.close();
+	this->file.open(this->path.c_str());
+	std::string tmp;
+	while (!this->file.eof())
+	{
+		std::getline(file, tmp);
+		std::cout << tmp << "\n";
 	}
 }
 
 void c_WorkFiles::revert()
 {
-	if (this->file.is_open()) {
-		std::string tmp;
-		char ch;
+	this->file.close();
+	this->file.open(this->path.c_str());
+	std::string tmp;
+	char ch;
 
-		while (this->file.get(ch)) {
-			tmp += ch;
-		}
-
-		for (size_t i = 0; i < tmp.length() /2; i++) {
-			std::swap(tmp[i], tmp[tmp.length() - i - 1]);
-		}
-
-		this->file.clear();
-		this->file.seekg(0, std::ios::beg);
-
-		this->file << tmp;
+	while (this->file.get(ch)) {
+		tmp += ch;
 	}
+
+	for (size_t i = 0; i < tmp.length() /2; i++) {
+		std::swap(tmp[i], tmp[tmp.length() - i - 1]);
+	}
+
+	this->file.close();
+	this->file.open(this->path.c_str());
+
+	this->file << tmp;
+	
 }
